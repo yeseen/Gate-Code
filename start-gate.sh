815592a7628faa82848e59ee7729f2af8380853c
EXC <gate@localhost.localdomain>
2019-02-26 01:08:59 +0000

    using wifi smart plug instead of stupid usb powerstrip

--- start-gate.sh
+++ start-gate.sh
@@ -1,26 +1,30 @@
 #!/bin/bash 
 killall -9 supervise 2>/dev/null # kill the deamon
-BUS=$(lsusb | grep '04d8:003f' | awk '{print $2}') # get the bus number for power strip
-DEVICE=$(lsusb | grep '04d8:003f' | awk '{print $4}' | sed 's/\([0-9]*\).*/\1/') # get the device number for power strip
+
+touch /home/gate/gatelogs/gate.log #make sure gate log is there
+gateIP=$(/home/gate/hs100/hs100.sh discover | awk 'NF{ print $NF }') # get smart plug ip
+
 EVENTNUM=$(ls -l /dev/input/by-id/ |  grep event | grep -v -i keyboard | awk '{print $11}' | cut -d"/" -f2) # get event number for scanner
-if [ -n "${BUS}" ] && [ -n "${EVENTNUM}" ]
+if [ -n "${gateIP}" ] && [ -n "${EVENTNUM}" ]
   then
-    # reset the USB-controlled power strip first because it keeps bugging uughhh
-    echo "Reseting PowerUSB with the following values"
-    echo "Bus:" $BUS "    Device:" $DEVICE "    EventNum:" $EVENTNUM 
-    echo xnqpla29 | sudo -S /home/blake/excursion/yassine/reset /dev/bus/usb/$BUS/$DEVICE  # reset USB-controlled power strip
+    echo "Scanner found at EventNum:" $EVENTNUM 
     echo xnqpla29 | sudo -S chmod 666 /dev/input/$EVENTNUM  #get persmission to access scanner
-    echo /dev/input/$EVENTNUM > /home/blake/excursion/yassine/deamon/scannereventfilename.txt # deamon/run will read this file for the filename
-    nohup supervise /home/blake/excursion/yassine/deamon &>/dev/null & # this runs the code in deamon/run and keep it running
+    echo /dev/input/$EVENTNUM > /home/gate/Gate-Code/deamon/scannereventfilename.txt # deamon/run will read this file for the filename
+
+    echo "Smart plug found at IP " $gateIP
+    echo xnqpla29 | sudo -S sed -i "\$ a $gateIP\ths100\n" /etc/hosts #add smart plug to known hosts
+
+    nohup supervise /home/gate/Gate-Code/deamon &>/dev/null & # this runs the code in deamon/run and keep it running
 fi
+if [ -z "${gateIP}" ]
+  then
+    echo "Can't find gate smart plug!! its probably not plugged" 1>&2
+    exit 1
+fi
+
 if [ -z "${EVENTNUM}" ]
   then
     echo "Can't find gate scanner!! Gate scanner probably not plugged" 1>&2
     exit 1
 fi
 
-if [ -z "${BUS}" ]
-  then
-    echo "Can't find BUS number!! USB-controlled power strip probably not plugged" 1>&2
-    exit 1
-fi 
