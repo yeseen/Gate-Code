4a5bb61a935cb2e220a4af938f6a75611f67bb0f
Yassine Dhane <yassinedhane@gmail.com>
2019-03-13 14:29:01 -0700

    generalize paths

--- start-gate.sh
+++ start-gate.sh
@@ -1,20 +1,20 @@
 #!/bin/bash 
 killall -9 supervise 2>/dev/null # kill the deamon
 
-touch /home/gate/gatelogs/gate.log #make sure gate log is there
-gateIP=$(/home/gate/hs100/hs100.sh discover | awk 'NF{ print $NF }') # get smart plug ip
+touch ~/gatelogs/gate.log #make sure gate log is there
+gateIP=$(~/hs100/hs100.sh discover | awk 'NF{ print $NF }') # get smart plug ip
 
 EVENTNUM=$(ls -l /dev/input/by-id/ |  grep event | grep -v -i keyboard | awk '{print $11}' | cut -d"/" -f2) # get event number for scanner
 if [ -n "${gateIP}" ] && [ -n "${EVENTNUM}" ]
   then
     echo "Scanner found at EventNum:" $EVENTNUM 
     echo xnqpla29 | sudo -S chmod 666 /dev/input/$EVENTNUM  #get persmission to access scanner
-    echo /dev/input/$EVENTNUM > /home/gate/Gate-Code/deamon/scannereventfilename.txt # deamon/run will read this file for the filename
+    echo /dev/input/$EVENTNUM > ~/Gate-Code/deamon/scannereventfilename.txt # deamon/run will read this file for the filename
 
     echo "Smart plug found at IP " $gateIP
     echo xnqpla29 | sudo -S sed -i "\$ a $gateIP\ths100\n" /etc/hosts #add smart plug to known hosts
 
-    nohup supervise /home/gate/Gate-Code/deamon &>/dev/null & # this runs the code in deamon/run and keep it running
+    nohup supervise ~/Gate-Code/deamon &>/dev/null & # this runs the code in deamon/run and keep it running
 fi
 if [ -z "${gateIP}" ]
   then
